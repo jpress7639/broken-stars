110e74e505c8bf2093fd16d44b161a184a3f6b7c
Jake Pressman <jpress7639@gmail.com>
2020-08-10 16:35:19 -0400

    hiding database

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,11 +2,15 @@ class Artist < ApplicationRecord
     has_many :symptoms
     has_many :resources
 
-    validates :name, presence: true, uniqueness: true 
+    validates :name, presence: true, uniqueness: true
     validates :profile_img, presence: true, uniqueness: true
     validates :years, presence: true, uniqueness: true
     validates :disorder, presence: true, uniqueness: true
     validates :work_example, presence: true, uniqueness: true
     validates :work_URL, presence: true, uniqueness: true
-    validates :story, presence: true, uniqueness: true
+    validates :story, presence: true, uniqueness: true, length: { maximum: 1000 }
+
+    # def name_validation
+    #     errors.add(:name, :invalid_characters, not_allowed: "!@#%")
+    # end
 end
